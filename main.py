4ce6e0e3a79e57d38eea26b1f74678b2acac63ee
Adill Al-Ashgar <adillwmaa@gmail.com>
2023-11-29 02:06:01 +0000

    Fixed bugs on backend, image renders properly without extra patches

--- main.py
+++ main.py
@@ -4,25 +4,36 @@ import os
 
 
 if __name__ == "__main__":
-    # specify local image path
-    local_image_paths = [r"A:\Users\Ada\Pictures\EN2wgxqW4AAtwLc.jpg"]  # replace with your actual image path
-    output_dir = r"output_data/"
-    prompt = ""
-    negative_prompt = ""
-    num_inference_steps = 10   # Must be > 3 for any usable output, > 5 for okay output, > 10 for good output, 50+ for great output
-    guidance_scale = 0.5
-    patch_size = 120
-    padding_size = 8
-    use_tensorboard = False
-    show_patches = False
-    dummy_upscale = False              # For debugging. If True, will not use the neural net to upscale image, instead a very very fast bicubic upscale is used, to speed up testing the rest of the code. Always set to False for actual usage.
+    local_image_paths = [r"A:\Users\Ada\Desktop\t3.jpg", r"A:\Users\Ada\Desktop\t4.jpg", r"A:\Users\Ada\Desktop\t5.jpg", r"A:\Users\Ada\Desktop\t6.jpg"]      # specify local image paths
+    output_dir = r"output_data/"           # specify output directory
+    prompt = ""                            # specify prompt for image upscaling
+    negative_prompt = ""                   # specify negative prompt for image upscaling
+    num_inference_steps = 25                # Must be > 3 for any usable output, > 5 for okay output, > 10 for good output, 50+ for great output
+    guidance_scale = 0.5          
+    patch_size = 120         
+    padding_size = 8               
+    callback_steps = 100                    # Number of steps between each callback. Set to 0 to disable callbacks. Callbacks are used to show the progress of the image upscaling.
+    blending = False                        # If True, will use soft blend. If False, will use hard blend. 
+    blend_mode = "add"            
+    
+    # Pipeline settings
+    xformers = False                       # If True, will use the xformers model. If False, will use the original model.
+    cpu_offload = False                    # If True, will use the CPU for the first few inference steps, then switch to the GPU. If False, will use the GPU for all inference steps. If True, will be slower but will use less GPU memory.
+    attention_slicing = False              # If True, will use attention slicing. If False, will not use attention slicing. If True, will be slower but will use less GPU memory.
 
-    upscale = upscaler(use_tensorboard=use_tensorboard)
+    # Debugging settings
+    show_patches = False                   # If True, will show the patches that are being upscaled. If False, will not show the patches that are being upscaled.
+    dummy_upscale = False                  # For debugging. If True, will not use the neural net to upscale image, instead a very very fast bicubic upscale is used, to speed up testing the rest of the code. Always set to False for actual usage.
+    use_tensorboard = False                # If True, will use tensorboard to log the progress of the image upscaling. If False, will not use tensorboard. 
+    seed = None                            # If None, will use a random seed. If set to a numerical value, will use value as the generator seed.
+
+
+    upscale = upscaler(xformers, cpu_offload, attention_slicing, seed, use_tensorboard)
     os.makedirs(output_dir, exist_ok=True)
 
     for local_image_path in local_image_paths:
             
-        upscaled_image = upscale.upscale(local_image_path, patch_size, padding_size, num_inference_steps, guidance_scale, prompt, negative_prompt, show_patches, dummy_upscale)
+        upscaled_image = upscale.upscale(local_image_path, patch_size, padding_size, num_inference_steps, guidance_scale, prompt, negative_prompt, blending, callback_steps, show_patches, dummy_upscale)
         
         # get input image name
         image_name = os.path.basename(local_image_path)
--- upscalar_class.py
+++ upscalar_class.py
@@ -8,12 +8,13 @@ from torchvision import transforms
 import matplotlib.pyplot as plt
 import math
 from PIL import Image, ImageChops, ImageEnhance
+from tqdm import tqdm
 
-### Fix seams by using a stride less than the patch size so that patches overlap and then cropping the overlap 'bleed' areas when recombining.
+### beacuse we are checking if file is greater than patch without padding when a file is exactly of windows size and there is only one pathc then we end up genrating four for no reason
 
-### invcesitgate fading the padding areas together vs hard cut
+### callback images washed out/odd?
 
-### Fix balck bar on bottom due to padding and the fact that the last patch is not the same size as the others
+### fix all blending modes. currently only normal works
 
 class upscaler():
     def __init__(self, xformers=False, cpu_offload=False, attention_slicing=False, seed=None, use_tensorboard=False):
@@ -35,6 +36,11 @@ class upscaler():
         if self.use_tensorboard:
             self.log_dir = "tensorboard_logs"
             os.makedirs(self.log_dir, exist_ok=True)
+            self.hparams_dict = {"xformers": xformers, 
+                                 "cpu_offload": cpu_offload, 
+                                 "attention_slicing": attention_slicing, 
+                                 "seed": seed,
+                                 "device": self.device}
 
     def callback(self, iter, t, latents):
         # convert latents to image
@@ -52,46 +58,18 @@ class upscaler():
 
             # do something with the Images
             for i, img in enumerate(image):
-                img.save(f"iter_{iter}_img{i}.png")
-
-    def split_image_into_patchesOLD(self, image):
-        # rather than the following tow being set directly there shoudl always be a sum of 128 and then the padding size should be chosen which will then take away from that 128 leaving the patch_width as the remainder
-        padding_size = 8                      # Pixels of padding on right and bottom sides of the patches
-        patch_size = 128 - padding_size        # Size of the patches to be extracted from the image in pixels
-        input_image_width, input_image_height = image.size
-    
-        number_of_patches_in_row = np.ceil(input_image_width / patch_size).astype(int)
-        number_of_patches_in_col = np.ceil(input_image_height / patch_size).astype(int)
-        print(f"Number of patches in row: {number_of_patches_in_row}")
-        print(f"Number of patches in col: {number_of_patches_in_col}")
-        print(f"Patch size: {patch_size}")
-
-        patches = []
-        for y in range(0, input_image_height, patch_size):
-            for x in range(0, input_image_width, patch_size):
-                # Crop the patch from the image
-                patch = image.crop((x, y, x + patch_size + padding_size, y + patch_size + padding_size))
-                print(f"Patch size: {patch.size}")
-                # Append the patch to the list
-                patches.append(patch)
-
-        return patches, number_of_patches_in_row, number_of_patches_in_col, patch_size
+                img.save(f"callbacks\patch_{self.patch_num}_iter_{iter}_img{i}.png")
 
     def calculate_dynamic_overlap(self, x, window_size, patch_size):
         blocks = int(np.ceil(x / patch_size))
-        hangover = (window_size * blocks) - x
+        hangover = (patch_size * blocks) - x
         num_of_overlaps = (blocks * 2) - 2
         overlap = hangover / num_of_overlaps                        # length hanging over = total length of blocks end to end - length of x                     number of overlaps = number of blocks * 2  - 2 as there are 2 overlaps for every block except the first and last which only have 1. if there is only 1 block then there is no overlap
         
         # round down overlap  
         overlap = math.floor(overlap)
-
         all_but_one_ol = overlap * (num_of_overlaps - 1)
         last_ol = hangover - all_but_one_ol   # to make sure all are ints and there is no remainder
-
-        print("overlap: ", overlap)
-        print("last overlap: ", last_ol)
-        print("all but one overlap: ", all_but_one_ol)
 
         return overlap, last_ol, blocks
 
@@ -101,7 +79,7 @@ class upscaler():
         min_padding_size = 8                      # Pixels of padding on right and bottom sides of the patches
         patch_size = window_size - min_padding_size        # Size of the patches to be extracted from the image in pixels
         input_image_width, input_image_height = image.size
-        
+
         x_overlap, x_last_overlap, number_of_windows_in_row = self.calculate_dynamic_overlap(input_image_width, window_size, patch_size)
         y_overlap, y_last_overlap, number_of_windows_in_col = self.calculate_dynamic_overlap(input_image_height, window_size, patch_size)
 
@@ -118,56 +96,14 @@ class upscaler():
                 else:
                     y_start_point = (c * window_size) - (c * y_overlap * 2)
 
-
-
-
                 # Crop the patch from the image
                 patch = image.crop((x_start_point, y_start_point, x_start_point + window_size, y_start_point + window_size))
 
                 # Append the patch to the list
                 patches.append(patch)
-
         return patches, number_of_windows_in_row, number_of_windows_in_col, patch_size, x_overlap, y_overlap, x_last_overlap, y_last_overlap
         
-    def reconstruct_from_patches(self, patches, number_of_patches_in_row, number_of_patches_in_col, patch_size, scaling_factor, x_overlap, y_overlap, x_last_overlap, y_last_overlap, original_image_shape):
-        scaling_factor = int(scaling_factor)
-        window_size = 128 * scaling_factor
-        min_padding_size = 8 * scaling_factor                                # Pixels of padding on right and bottom sides of the patches
-        patch_size = window_size - min_padding_size                          # Size of the patches to be extracted from the image in pixels
-
-        # calculate the size of the reconstructed image
-        width = original_image_shape[0] * scaling_factor
-        height = original_image_shape[1] * scaling_factor
-
-        # Create a new image with the same mode and size as the original image
-        reconstructed_image = Image.new(mode='RGB', size=[int(width), int(height)])
-
-        # Paste each patch onto the result image
-        for c in range(number_of_patches_in_col):
-            for r in range(number_of_patches_in_row):
-                if r == number_of_patches_in_row - 1:
-                    x_start_point = (r * window_size) - (r * x_last_overlap  * scaling_factor * 2)
-                else:
-                    x_start_point = (r * window_size) - (r * x_overlap * scaling_factor * 2)
-                if c == number_of_patches_in_col - 1:
-                    y_start_point = (c * window_size) - (c * y_last_overlap * scaling_factor * 2)
-                else:
-                    y_start_point = (c * window_size) - (c * y_overlap * scaling_factor * 2)
-
-                # Paste the patch onto the result image
-                reconstructed_image.paste(patches[c * number_of_patches_in_row + r], (x_start_point, y_start_point))
-
-        return reconstructed_image
-
-    def blend_images(self, background, overlay, position):
-        # Calculate the alpha value based on the overlap
-        alpha = overlay.convert('L')  # Convert to grayscale
-        alpha = ImageEnhance.Brightness(alpha).enhance(0.5)  # Adjust the brightness as needed
-
-        # Paste the overlay onto the background with blending
-        background.paste(overlay, position, mask=alpha)
-
-    def reconstruct_from_patches_w_blending(self, patches, number_of_patches_in_row, number_of_patches_in_col, patch_size, scaling_factor, x_overlap, y_overlap, x_last_overlap, y_last_overlap, original_image_shape, blending=True):
+    def reconstruct_from_patches(self, patches, number_of_patches_in_row, number_of_patches_in_col, patch_size, scaling_factor, x_overlap, y_overlap, x_last_overlap, y_last_overlap, original_image_shape, blending):
         scaling_factor = int(scaling_factor)
         window_size = 128 * scaling_factor
         min_padding_size = 8 * scaling_factor                                # Pixels of padding on right and bottom sides of the patches
@@ -201,6 +137,38 @@ class upscaler():
 
         return reconstructed_image
 
+    def blend_images(self, background, overlay, position, blending_mode="normal"):
+        # Calculate the alpha value based on the overlap
+        alpha = overlay.convert('L')  # Convert to grayscale
+        alpha = ImageEnhance.Brightness(alpha).enhance(0.5)  # Adjust the brightness as needed
+
+        # Apply blending mode based on user selection
+        if blending_mode == "normal":
+            blended_image = overlay
+        elif blending_mode == "add":
+            blended_image = ImageChops.add(background, overlay, scale=2.0)
+        elif blending_mode == "multiply":
+            blended_image = ImageChops.multiply(background, overlay)
+        elif blending_mode == "screen":
+            blended_image = ImageChops.screen(background, overlay)
+        elif blending_mode == "overlay":
+            blended_image = ImageChops.overlay(background, overlay)
+        elif blending_mode == "soft_light":
+            blended_image = ImageChops.soft_light(background, overlay)
+        elif blending_mode == "hard_light":
+            blended_image = ImageChops.hard_light(background, overlay)
+        elif blending_mode == "darken":
+            blended_image = ImageChops.darker(background, overlay)
+        elif blending_mode == "lighten":
+            blended_image = ImageChops.lighter(background, overlay)
+        elif blending_mode == "difference":
+            blended_image = ImageChops.difference(background, overlay)
+        elif blending_mode == "exclusion":
+            blended_image = ImageChops.invert(ImageChops.difference(background, overlay))
+
+        # Paste the blended image onto the result image
+        background.paste(blended_image, position)
+
     def visualize_patches(self, patches, number_of_windows_in_row, number_of_windows_in_col):
         # Calculate the number of rows and columns for the grid
         num_cols = number_of_windows_in_col
@@ -226,23 +194,23 @@ class upscaler():
     def update_animation(self, upscaled_patches):
         pass
 
-    def upscale(self, local_image_path, patch_size=120, padding_size=8, num_inference_steps=10, guidance_scale=0.5, prompt="", negative_prompt="", callback_steps=1, show_patches=False, dummy_upscale=False):
+    def upscale(self, local_image_path, patch_size=120, padding_size=8, num_inference_steps=10, guidance_scale=0.5, prompt="", negative_prompt="", blending=True, callback_steps=1, show_patches=False, dummy_upscale=False):
         # specify local image path
         low_res_img = Image.open(local_image_path).convert("RGB")
 
         #  Split the input image into patches of shape (patch_size, patch_size), if there are patches of the image which do not fit into the patch grid, flip the image and continue it from the edge of the image to fill the patch grid.  
         patches, number_of_windows_in_row, number_of_windows_in_col, patch_size, x_overlap, y_overlap, x_last_overlap, y_last_overlap = self.split_image_into_patches(low_res_img)
-
-        print("number of patches: ", len(patches))
+        print("number of patches to process: ", len(patches))
 
         upscaled_patches = []
-        for i, patch in enumerate(patches):
+        for patch_num, patch in tqdm(enumerate(patches), total=len(patches), desc="Upscaling patches", leave=True, unit="patch", colour='green'):
 
             if dummy_upscale:
                 # strecth the patch to 4x its size
                 upscaled_patch = patch.resize(((patch_size + padding_size) * 4, (patch_size + padding_size) * 4), Image.BICUBIC)
             
             else:
+                self.patch_num = patch_num
                 upscaled_patch = self.pipeline(prompt=prompt, 
                                                 image=patch, 
                                                 num_inference_steps=num_inference_steps, 
@@ -260,21 +228,22 @@ class upscaler():
             self.visualize_patches(upscaled_patches, number_of_windows_in_row, number_of_windows_in_col)
 
         scaling_factor = upscaled_patches[0].size[0] / patches[0].size[0]   # calculate the scaling factor from the size of the first upscaled patch and the first patch
-        upscaled_image = self.reconstruct_from_patches(upscaled_patches, number_of_windows_in_row, number_of_windows_in_col, patch_size, scaling_factor, x_overlap, y_overlap, x_last_overlap, y_last_overlap, low_res_img.size)
+        upscaled_image = self.reconstruct_from_patches(upscaled_patches, number_of_windows_in_row, number_of_windows_in_col, patch_size, scaling_factor, x_overlap, y_overlap, x_last_overlap, y_last_overlap, low_res_img.size, blending)
         print("Upscale successful")
 
         if self.use_tensorboard:
             writer = SummaryWriter(log_dir=self.log_dir)
             writer.add_image("low_res_image", self.transform(low_res_img))      # Log the low-resolution image to TensorBoard
             writer.add_image("upscaled_image", self.transform(upscaled_image))                   # Log the upscaled image to TensorBoard
-            hparams_dict = {
-                "num_inference_steps": num_inference_steps,
-                "patch_size": patch_size,
-                "guidance_scale": guidance_scale,
-                "prompt": prompt,
-                "negative_prompt": negative_prompt
-            }  
-            writer.add_hparams(hparams_dict, {})                                                 # Log the hyperparameters to TensorBoard  
+            
+            
+            # Adding more entries later is as simple as:
+            self.hparams_dict["num_inference_steps"] = num_inference_steps
+            self.hparams_dict["patch_size"] = patch_size
+            self.hparams_dict["guidance_scale"] = guidance_scale
+            self.hparams_dict["prompt"] = prompt
+            self.hparams_dict["negative_prompt"] = negative_prompt
+            writer.add_hparams(self.hparams_dict, {})                                                 # Log the hyperparameters to TensorBoard  
             writer.close()                                                                       # Close the TensorBoard writer
 
         return upscaled_image
@@ -288,13 +257,14 @@ if __name__ == "__main__":
     guidance_scale = 0.5
     patch_size = 120
     padding_size = 8
-    use_tensorboard = True
+    use_tensorboard = False
     callback_steps = 1
+    blending = False
     show_patches = False
     dummy_upscale = False              # For debugging. If True, will not use the neural net to upscale image, instead a very very fast bicubic upscale is used, to speed up testing the rest of the code. Always set to False for actual usage.
 
     upscale = upscaler(use_tensorboard=use_tensorboard)
-    upscaled_image = upscale.upscale(local_image_path, patch_size, padding_size, num_inference_steps, guidance_scale, prompt, negative_prompt, callback_steps, show_patches, dummy_upscale)
+    upscaled_image = upscale.upscale(local_image_path, patch_size, padding_size, num_inference_steps, guidance_scale, prompt, negative_prompt, blending, callback_steps, show_patches, dummy_upscale)
     
     # copy input image to output location 
     low_res_img = Image.open(local_image_path).convert("RGB")
